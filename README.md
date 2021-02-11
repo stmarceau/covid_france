f49ce90aaabddf863343b6fae08f58758e01cab5
stmarceau <stmarceau@gmail.com>
2021-02-11 15:48:29 +0100

    model compare

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
 # study about french population and covid deaths
 In oder to understand all figures provided by the french government, I decided to check whether I could investigate by myself to figure out what could be the consequence of covid-19 on the french people casualties.
 
-To do so, I to 
-* build up population data frame with history from 2000
+To do so, I did
+* build up population data frame with history since 1990
 * carried out a french population history review to better understand the obeserved death rate growth
 * forecasted 2020 deaths based on previsous history
-* compare 2020 forecast with real death count considered as covid casualties
+* compared 2020 forecast with real death count considered as covid casualties
--- population fr.R
+++ population fr.R
@@ -18,12 +18,6 @@ popTs<-popStruct[,1:8]%>%
   as_tsibble(index=Année)%>%
   pivot_longer(cols=-c(1,2),names_to="Ages", values_to="Qty", values_transform=list(integer))
 
-#plot population per age bin  
-popTs %>%
-  group_by(Ages)%>%
-  summarise(Q=sum(Qty))%>%
-  autoplot(Q)
-
 # Population review
 popCroiss<-read_xlsx("sources/fe_dod_compo_crois.xlsx",skip = 2)
 popCroiss[is.na(popCroiss$...2),"...2"]<-""
@@ -40,17 +34,17 @@ pop<-popCroiss %>%
 
 # pop %>% str()
 pop[,10:15]<-mapply(FUN = '/', pop[,10:15], pop[,3])
-pop %>%
-  pivot_longer(cols=`15-19`:`75+`, names_to="Ages", values_to="Age_rate")%>%
-  select(Année, Ages, Age_rate)
 
+# plot death rate
 p1<-pop%>%
   ggplot()+
   geom_line(aes(x=Année, y=`mortalité_1M_hab`))
+# plot population
 p2<-pop %>%
   ggplot(aes(x=Année,y=`Population au 1er janvier`))+
   geom_line()
-p3<-pop%>%
+#plot population per age bin
+p3<-pop%>% 
   pivot_longer(cols=`15-19`:`75+`, names_to="Ages", values_to="Age_rate")%>%
   mutate(Ages=Ages, label=if_else(Année==max(Année),Ages,NA_character_))%>%
   ggplot(aes(x=Année,y=`Age_rate`, color=Ages))+
@@ -59,21 +53,18 @@ p3<-pop%>%
   scale_y_continuous(trans="log10")+
   ggrepel::geom_label_repel(aes(label = label), nudge_x = 1, na.rm = TRUE)
 
-
 library(patchwork)
 p1 / p2 / p3
 
-pop %>%
-  group_by(Ages)%>%
-  summarise(Q=sum(Qty))%>%
-  autoplot(Q)
-
-
+# time serie decomposition
 pop %>% features(mortalité_1M_hab,feat_stl)
+
+# ACL features
+pop %>% features(mortalité_1M_hab,feat_acf)
 
 fit_out<-pop %>% model(ETS(mortalité_1M_hab))
 report(fit_out)
-components(fit_out) %>% autoplot()
+augment(fit_out)
 
 # MA smoothing
 pop %>%
@@ -84,26 +75,33 @@ pop %>%
   geom_line(aes(x=Année,y=lag), color="red")+
   geom_line(aes(x=Année,y=mortalité_1M_hab), color="blue")
   
-
-# lag & ACF plot
-pop %>% gg_lag(`mortalité_1M_hab`, geom = "point")
+# ACF plot
 pop %>% ACF(`mortalité_1M_hab`) %>%autoplot()
+# death rate seems to be correlated with 1Y, 2Y, 3Y lagged data 
 
-# Arima
-fit<-pop%>%
-  filter(Année>2005 & Année<2020)%>%
-  model(arima=ARIMA(`mortalité_1M_hab` ~ 1 + pdq(0,0,3)))
+# forecasting
+pop_train<-pop %>%
+  slice(1:(n()-5))
+pop_test<-pop %>%
+  filter(Année>2015)
 
-report(fit)
-fit %>% forecast(h=3) %>% autoplot(pop)
+fit<-pop_train %>%
+  model(ets=ETS(mortalité_1M_hab ~ trend("A")+error("A")),
+        ets_auto=ETS(mortalité_1M_hab),
+        arima=ARIMA(`mortalité_1M_hab` ~ 1 + pdq(0,0,3)))
 
-predict(fit, pop)
+# residuals review
+fit %>% augment()%>% autoplot(.resid) 
+fit %>% augment()%>%
+  ggplot(aes(x=.innov))+
+  geom_histogram()+
+  facet_wrap(~.model,nrow=3)
 
-# ETS
-fit2<-pop%>%
-  filter(Année>2004 & Année<2020)%>%
-  model(ets=ETS(`mortalité_1M_hab` ~ trend("A")+error("A")))
+# model accuracy
+accuracy(fit)
 
-report(fit2)
-fit2 %>% forecast(h=3) %>% autoplot(pop)
+# forecast accuracy
+fit %>% forecast(h=5) %>% accuracy(pop_test)  
 
+# plot forecast
+fit %>% forecast(h=5) %>% autoplot(pop)
